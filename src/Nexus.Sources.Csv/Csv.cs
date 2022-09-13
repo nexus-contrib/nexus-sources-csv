703819359bc2a8bad1bb8ddaf3723009c917cf29
Apollo3zehn <git@m1.apollo3zehn.net>
2022-09-13 17:31:36 +0200

    Finish implementation.

--- src/Nexus.Sources.Csv/Csv.cs
+++ src/Nexus.Sources.Csv/Csv.cs
@@ -1,7 +1,12 @@
-﻿using Nexus.DataModel;
+﻿using Microsoft.Extensions.Logging;
+using Nexus.DataModel;
 using Nexus.Extensibility;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace Nexus.Sources
 {
@@ -16,9 +21,33 @@ namespace Nexus.Sources
             Dictionary<string, FileSource> FileSources, 
             JsonElement? AdditionalProperties);
 
+        record ReplaceNameRule(
+            string Pattern,
+            string Replacement);
+
+        record AdditionalProperties(
+            TimeSpan SamplePeriod,
+            string InvalidValue,
+            int CodePage,
+            int HeaderRow,
+            List<int>? SkipColumns,
+            string? UnitPattern,
+            List<ReplaceNameRule>? ReplaceNameRules,
+            char Separator = ',',
+            char DecimalSeparator = '.');
+
         #region Fields
 
         private Dictionary<string, CatalogDescription> _config = default!;
+
+        #endregion
+
+        #region Constructors
+
+        static Csv()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
 
         #endregion
 
@@ -58,36 +87,49 @@ namespace Nexus.Sources
 
             foreach (var (fileSourceId, fileSource) in catalogDescription.FileSources)
             {
-                var filePaths = default(string[]);
-                var catalogSourceFiles = fileSource.AdditionalProperties.GetStringArray("CatalogSourceFiles");
-
-                if (catalogSourceFiles is not null)
-                {
-                    filePaths = catalogSourceFiles
-                        .Where(filePath => filePath is not null)
-                        .Select(filePath => Path.Combine(Root, filePath!))
-                        .ToArray();
-                }
-                else
-                {
-                    if (!TryGetFirstFile(fileSource, out var filePath))
-                        continue;
-
-                    filePaths = new[] { filePath };
-                }
-
                 cancellationToken.ThrowIfCancellationRequested();
 
-                foreach (var filePath in filePaths)
+                var newCatalogBuilder = new ResourceCatalogBuilder(id: catalogId);
+
+                if (fileSource.AdditionalProperties is null)
+                    continue;
+
+                var additionalProperties = JsonSerializer
+                    .Deserialize<AdditionalProperties>(fileSource.AdditionalProperties.Value)!;
+
+                if (!TryGetFirstFile(fileSource, out var filePath))
+                    continue;
+
+                var encoding = Encoding.GetEncoding(additionalProperties.CodePage);
+                using var reader = new StreamReader(File.OpenRead(filePath), encoding);
+
+                var resourceProperties = GetResourceProperties(reader, additionalProperties);
+
+                foreach (var resourceProperty in resourceProperties)
                 {
-                    var resources = GetResources(fileSourceId);
+                    if (resourceProperty.Equals(default((string, string?))))
+                        continue;
 
-                    var newCatalog = new ResourceCatalogBuilder(id: catalogId)
-                        .AddResources(resources)
-                        .Build();
+                    var (resourceId, unit) = resourceProperty;
 
-                    catalog = catalog.Merge(newCatalog);
+                    // build representation
+                    var representation = new Representation(
+                        dataType: NexusDataType.FLOAT64,
+                        samplePeriod: additionalProperties.SamplePeriod);
+
+                    // build resource
+                    var resourceBuilder = new ResourceBuilder(id: resourceId)
+                        .WithGroups(fileSourceId)
+                        .WithProperty(FileSourceKey, fileSourceId)
+                        .AddRepresentation(representation);
+
+                    if (unit is not null)
+                        resourceBuilder.WithUnit(unit);
+
+                    newCatalogBuilder.AddResource(resourceBuilder.Build());
                 }
+
+                catalog = catalog.Merge(newCatalogBuilder.Build());
             }
 
             return Task.FromResult(catalog);
@@ -97,13 +139,148 @@ namespace Nexus.Sources
         {
             return Task.Run(() =>
             {
-                // to be implemented
+                if (info.FileSource.AdditionalProperties is null)
+                    return;
+
+                var additionalProperties = JsonSerializer
+                    .Deserialize<AdditionalProperties>(info.FileSource.AdditionalProperties.Value)!;
+
+                // number format info
+                var nfi = new NumberFormatInfo()
+                {
+                    NumberDecimalSeparator = additionalProperties.DecimalSeparator.ToString()
+                };
+
+                // encoding / reader
+                var encoding = Encoding.GetEncoding(additionalProperties.CodePage);
+                using var reader = new StreamReader(File.OpenRead(info.FilePath), encoding);
+
+                // resource properties
+                var resourceProperties = GetResourceProperties(reader, additionalProperties);
+
+                // find index
+                var index = resourceProperties
+                    .FindIndex((resourceProperty => resourceProperty.Item1 == info.CatalogItem.Resource.Id));
+
+                if (index > -1)
+                {
+                    // seek
+                    for (int i = 0; i < info.FileOffset; i++)
+                    {
+                        reader.ReadLine();
+                    }
+
+                    // read
+                    var buffer = new double[info.FileBlock];
+                    var byteBuffer = MemoryMarshal.AsBytes(buffer.AsSpan());
+
+                    for (int i = 0; i < info.FileBlock; i++)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line is null)
+                        {
+                            Logger.LogDebug("The actual buffer size does not match the expected size, which indicates an incomplete file");
+                            return;
+                        }
+
+                        var parts = line.Split(additionalProperties.Separator, index + 2);
+
+                        if (parts.Length < (index + 1))
+                        {
+                            Logger.LogDebug("The actual buffer size does not match the expected size, which indicates an incomplete file");
+                            return;
+                        }
+
+                        var cell = parts[index];
+
+                        if (cell == additionalProperties.InvalidValue)
+                        {
+                            buffer[i] = double.NaN;
+                        }
+
+                        else
+                        {
+                            if (!double.TryParse(cell, NumberStyles.Float, nfi, out var value))
+                                value = double.NaN;
+
+                            buffer[i] = value;
+                        }
+                    }
+
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    // write data
+                    byteBuffer
+                        .CopyTo(info.Data.Span);
+
+                    info
+                        .Status
+                        .Span
+                        .Fill(1);
+                }
             });
         }
 
-        private List<Resource> GetResources(string fileSourceId)
+        private List<(string, string?)> GetResourceProperties(
+            StreamReader reader,
+            AdditionalProperties additionalProperties)
         {
-            throw new NotImplementedException();
+            // read header line
+            for (int i = 0; i < additionalProperties.HeaderRow; i++)
+            {
+                var skippedLine = reader.ReadLine();
+
+                if (skippedLine is null)
+                    throw new Exception("The file is incomplete.");
+            }
+
+            var headerLine = reader.ReadLine();
+
+            if (headerLine is null)
+                throw new Exception("The file is incomplete.");
+
+            // analyse header line
+            var resourceProperties = new List<(string, string?)>();
+            var columns = headerLine.Split(additionalProperties.Separator);
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                // skip columns
+                var column = columns[i];
+
+                if (additionalProperties.SkipColumns is not null &&
+                    additionalProperties.SkipColumns.Contains(i))
+                {
+                    resourceProperties.Add(default);
+                    continue;
+                }
+
+                // try get unit
+                var unit = default(string?);
+
+                if (additionalProperties.UnitPattern is not null)
+                {
+                    var match = Regex.Match(column, additionalProperties.UnitPattern);
+
+                    if (match.Success)
+                        unit = match.Groups[1].Value;
+                }   
+
+                // try get resource id
+                var resourceId = FormatResourceId(column, additionalProperties.ReplaceNameRules);
+
+                if (!TryEnforceNamingConvention(resourceId, out resourceId))
+                {
+                    resourceProperties.Add(default);
+                    continue;
+                }
+
+                // 
+                resourceProperties.Add((resourceId, unit));
+            }
+
+            return resourceProperties;
         }
 
         private bool TryEnforceNamingConvention(string resourceId, [NotNullWhen(returnValue: true)] out string newResourceId)
@@ -113,6 +290,19 @@ namespace Nexus.Sources
             newResourceId = Resource.InvalidIdStartCharsExpression.Replace(newResourceId, "");
 
             return Resource.ValidIdExpression.IsMatch(newResourceId);
+        }
+
+        private string FormatResourceId(string id, List<ReplaceNameRule>? replaceNameRules)
+        {
+            if (replaceNameRules is not null)
+            {
+                foreach (var rule in replaceNameRules)
+                {
+                    id = Regex.Replace(id, rule.Pattern, rule.Replacement);
+                }
+            }
+
+            return id;
         }
 
         #endregion
--- tests/Nexus.Sources.Csv.Tests/CsvTests.cs
+++ tests/Nexus.Sources.Csv.Tests/CsvTests.cs
@@ -1,3 +1,7 @@
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Logging.Abstractions;
+using Nexus.DataModel;
+using Nexus.Extensibility;
 using Xunit;
 
 namespace Nexus.Sources.Tests
@@ -7,19 +11,110 @@ namespace Nexus.Sources.Tests
         [Fact]
         public async Task ProvidesCatalog()
         {
-            
+            // arrange
+            var dataSource = new Csv() as IDataSource;
+
+            var context = new DataSourceContext(
+                ResourceLocator: new Uri("Database", UriKind.Relative),
+                SystemConfiguration: default!,
+                SourceConfiguration: default!,
+                RequestConfiguration: default!);
+
+            await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
+
+            // act
+            var actual = await dataSource.GetCatalogAsync("/A/B/C", CancellationToken.None);
+            var actualIds = actual.Resources!.Select(resource => resource.Id).ToList();
+            var actualUnits = actual.Resources!.Select(resource => resource.Properties.GetStringValue("unit")).ToList();
+            var actualGroups = actual.Resources!.SelectMany(resource => resource.Properties.GetStringArray("groups")!).ToList();
+            var actualTimeRange = await dataSource.GetTimeRangeAsync("/A/B/C", CancellationToken.None);
+
+            // assert
+            var expectedIds = new List<string>() { "ThisIsTheBarVariable", "Anything" };
+            var expectedUnits = new List<string>() { "m/s", "°C" };
+            var expectedGroups = new List<string>() { "raw", "raw" };
+            var expectedStartDate = new DateTime(2020, 01, 01, 00, 00, 00);
+            var expectedEndDate = new DateTime(2020, 01, 01, 00, 00, 10);
+
+            Assert.True(expectedIds.SequenceEqual(actualIds));
+            Assert.True(expectedUnits.SequenceEqual(actualUnits));
+            Assert.True(expectedGroups.SequenceEqual(actualGroups));
+            Assert.Equal(expectedStartDate, actualTimeRange.Begin);
+            Assert.Equal(expectedEndDate, actualTimeRange.End);
         }
 
         [Fact]
-        public async Task ProvidesDataAvailability()
+        public async Task CanRead()
         {
-            
-        }
+            // arrange
+            var dataSource = new Csv() as IDataSource;
 
-        [Fact]
-        public async Task CanReadFullDay()
-        {
-            
+            var context = new DataSourceContext(
+                ResourceLocator: new Uri("Database", UriKind.Relative),
+                SystemConfiguration: default!,
+                SourceConfiguration: default!,
+                RequestConfiguration: default!);
+
+            await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
+
+            // act
+            var catalog = await dataSource.GetCatalogAsync("/A/B/C", CancellationToken.None);
+            var resource1 = catalog.Resources!.First();
+            var resource2 = catalog.Resources!.Last();
+            var representation1 = resource1!.Representations!.First();
+            var representation2 = resource2!.Representations!.First();
+
+            var catalogItem1 = new CatalogItem(catalog, resource1, representation1);
+            var catalogItem2 = new CatalogItem(catalog, resource2, representation2);
+
+            var begin = new DateTime(2020, 01, 01, 0, 0, 1, DateTimeKind.Utc);
+            var end = new DateTime(2020, 01, 01, 0, 0, 11, DateTimeKind.Utc);
+            var (data1, status1) = ExtensibilityUtilities.CreateBuffers(representation1, begin, end);
+            var (data2, status2) = ExtensibilityUtilities.CreateBuffers(representation2, begin, end);
+
+            var result1 = new ReadRequest(catalogItem1, data1, status1);
+            var result2 = new ReadRequest(catalogItem2, data2, status2);
+            await dataSource.ReadAsync(begin, end, new ReadRequest[] { result1, result2 }, default!, new Progress<double>(), CancellationToken.None);
+
+            // assert
+            void DoAssert()
+            {
+                // result 1
+                var data1 = MemoryMarshal.Cast<byte, double>(result1.Data.Span);
+
+                Assert.Equal(2e9, data1[0]);
+                Assert.Equal(-10.34e-3, data1[1]);
+                Assert.Equal(4, data1[2]);
+                Assert.Equal(5, data1[3]);
+                Assert.Equal(6.99, data1[4]);
+                Assert.Equal(7.99, data1[5]);
+                Assert.Equal(8.99, data1[6]);
+                Assert.Equal(9.99, data1[7]);
+                Assert.Equal(10.99, data1[8]);
+
+                Assert.Equal(1, result1.Status.Span[0]);
+                Assert.Equal(1, result1.Status.Span[8]);
+                Assert.Equal(0, result1.Status.Span[9]);
+
+                // result 2
+                var data2 = MemoryMarshal.Cast<byte, double>(result2.Data.Span);
+
+                Assert.Equal(double.NaN, data2[0]);
+                Assert.Equal(2, data2[1]);
+                Assert.Equal(3, data2[2]);
+                Assert.Equal(double.NaN, data2[3]);
+                Assert.Equal(5, data2[4]);
+                Assert.Equal(6, data2[5]);
+                Assert.Equal(7, data2[6]);
+                Assert.Equal(8, data2[7]);
+                Assert.Equal(9, data2[8]);
+
+                Assert.Equal(1, result2.Status.Span[0]);
+                Assert.Equal(1, result2.Status.Span[8]);
+                Assert.Equal(0, result2.Status.Span[9]);
+            }
+
+            DoAssert();
         }
     }
 }
